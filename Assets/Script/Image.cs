766af8af418ec8bf483714da70cf2199c1340395
Iannis Poquet <ianpo@live.fr>
2024-07-11 20:17:54 +0200

    Initial Commit

---
+++ Assets/Script/Image.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+[Serializable]
+public struct Image : IComparable<Image>
+{
+    public Image(string name, float dominantHue, float averageHue)
+    {
+        Name = name;
+        DominantHue = dominantHue;
+        AverageHue = averageHue;
+    }
+    
+    public Image(string name, string dominantHue, string averageHue)
+    {
+        Name = name;
+   
+        if (!float.TryParse(dominantHue, NumberStyles.Float, CultureInfo.InvariantCulture, out DominantHue))
+        {
+            DominantHue = -1;
+        }
+    
+        if (!float.TryParse(averageHue, NumberStyles.Float, CultureInfo.InvariantCulture, out AverageHue))
+        {
+            AverageHue = -1;
+        }
+    }
+
+    public string Name;
+    public float DominantHue;
+    public float AverageHue;
+
+    public int CompareTo(Image other)
+    {
+        var dominantHueComparison = DominantHue.CompareTo(other.DominantHue);
+        if (dominantHueComparison != 0) return dominantHueComparison;
+        return AverageHue.CompareTo(other.AverageHue);
+    }
+}
---
+++ Assets/Script/Visualizer.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using UnityEngine;
+using yutokun;
+
+
+
+public class Visualizer : MonoBehaviour
+{
+    [SerializeField] private string m_CSVPath;
+    [SerializeField, Min(0)] private float m_RadiusMin = 5;
+    [SerializeField, Min(0)] private float m_RadiusMax = 15;
+    [SerializeField] private float m_ImageSize = 0.1f;
+    
+    private const int c_DominateHueIndex = 6;
+    private List<Image> m_Images;
+    private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+    private const int c_AverageHueIndex = 3;
+    private const int c_NamesIndex = 0;
+
+    private void PrepareFromCsv()
+    {
+        List<List<string>> csv = CSVParser.LoadFromPath(m_CSVPath, Delimiter.Comma);
+        // Line 0 is the header.
+        {
+            Debug.Log("Names :" + csv[0][c_NamesIndex]);
+            Debug.Log("Average Hue Index :" + csv[0][c_AverageHueIndex]);
+            Debug.Log("Dominate Hue Index :" + csv[0][c_DominateHueIndex]);
+        }
+        m_Images ??= new(csv.Count);
+        for (int line = 1; line < csv.Count; line++)
+        {
+            m_Images.Add(new Image(csv[line][c_NamesIndex], csv[line][c_DominateHueIndex], csv[line][c_AverageHueIndex]));
+        }
+        Debug.Log("Parsing of necessary data finished");
+    }
+
+    private void PreprocessData()
+    {
+        m_Images.Sort();
+
+        foreach (var image in m_Images)
+        {
+            Debug.Log($"{image.Name}: d={image.DominantHue}, a={image.AverageHue}");
+        }
+    }
+
+    private void GenerateImages()
+    {
+        for (int i = 0; i < m_Images.Count; i++)
+        {
+            var image = m_Images[i];
+            string resourceStr = "images/" + image.Name;
+            resourceStr = resourceStr.Remove(resourceStr.Length - 4, 4);
+            Texture tex = Resources.Load<Texture>(resourceStr);
+            
+            var degree = ((float)i / (float)m_Images.Count) * (360.0f);
+            Quaternion rot = Quaternion.AngleAxis(degree, Vector3.up);
+            Vector3 fwd = rot * Vector3.forward;
+            
+
+            float distance = Mathf.Lerp(m_RadiusMin, m_RadiusMax, image.AverageHue);
+            fwd *= distance;
+            var quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            quad.gameObject.name = image.Name;
+            quad.transform.rotation = Quaternion.LookRotation(Vector3.down, rot * Vector3.forward);
+            quad.transform.position = fwd;
+            quad.transform.localScale = new Vector3(m_ImageSize, m_ImageSize, m_ImageSize);
+
+            quad.GetComponent<MeshRenderer>().material = new Material(Shader.Find("Standard"));
+            quad.GetComponent<MeshRenderer>().material.SetTexture(MainTex, tex);;
+            
+            
+            quad.transform.SetParent(this.transform, true);
+        }
+    }
+    
+    private void Start()
+    {
+        PrepareFromCsv();
+        
+        PreprocessData();
+
+        GenerateImages();
+    }
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
